cff78504aa3462d1be563ba53935b54f507ff8b8
Ryan Sturmer <ryansturmer@gmail.com>
2012-01-13 04:49:20 +0000

    Added hit testing for empty spaces. Tilebox now supports adding of new tiles. Proper game mechanics for placement of tiles (no tile depletion support)

--- hive.py
+++ hive.py
@@ -24,8 +24,9 @@ TILE_COLORS = { TILE_QUEEN:(0xfc,0xe9,0x4f),
                 TILE_SPIDER:(0xc1,0x7d,0x11)}
 
 HIT_TYPE_MISS = 0
-HIT_TYPE_PLAY = 1
+HIT_TYPE_TILE = 1
 HIT_TYPE_TILEBOX = 2
+HIT_TYPE_EMPTY = 3
 
 MIN_SCALE = 0.2
 MAX_SCALE = 10.0
@@ -47,10 +48,34 @@ class HiveGame(object):
         self.hilighted_tile = None
         self.setup_game()
 
-    def place_new_tile(self, loc, tile):
-        if self.turn = 0:
-            self.tiles[loc] = tile
-    
+    def place_new_tile(self, loc, tile_type):
+        'Actually make a move.  Places a tile for the current player, if legal.'
+        if loc in self.get_legal_place_locs():
+            self.add_tile(loc, Tile(self.player, tile_type))
+        self.turn += 1
+        self.player_tiles[self.player][tile_type] -= 1
+        self.player = PLAYER_2 if self.player == PLAYER_1 else PLAYER_1
+
+    def get_legal_place_locs(self, player=None):
+        if player == None: player = self.player
+        if self.turn == 0:
+            return [(0,0)]
+        if self.turn == 1:
+            return hex_neighbors(0,0)
+
+        boundary_spaces = self.get_legal_locs()
+        retval = []
+        for space in boundary_spaces:
+            neighbors = hex_neighbors(*space)
+            ok = True
+            for neighbor in neighbors:
+                if neighbor in self.tiles and self.tiles[neighbor][-1].player != player:
+                    ok = False
+                    break
+            if ok:
+                retval.append(space)
+        return retval
+
     def setup_game(self):    
         self.player = PLAYER_1
         self.player_tiles = {}
@@ -88,11 +113,7 @@ class HiveGame(object):
         a,b = neighbors[(i+1) % 6], neighbors[i-1]
         return not(a in self.tiles and b in self.tiles)
     
-    def get_legal_moves(self, loc):
-        if loc not in self.tiles:
-            raise ValueError("Not a tile, you guys!")
-        tile = self.tiles[loc][-1]
-
+    def get_legal_locs(self):
         # Compute anywhere in the hive that anyone can move
         all_legal_spots = set()
         for (a,b), stack in self.tiles.items():
@@ -100,6 +121,13 @@ class HiveGame(object):
             unoccupied_neighbors = filter(lambda n : n not in self.tiles, neighbors)
             for n in unoccupied_neighbors:
                 all_legal_spots.add(n)
+        return all_legal_spots
+
+    def get_legal_moves(self, loc):
+        all_legal_spots = self.get_legal_locs()
+        if loc not in self.tiles:
+            raise ValueError("Not a tile, you guys!")
+        tile = self.tiles[loc][-1]
 
         if tile.type == TILE_QUEEN:
             # Moves one and only one space, along 
@@ -130,15 +158,13 @@ class HivePanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, -1)
         self.model = HiveGame()
-        self.model.add_tile((0,0), Tile())
-        self.model.add_tile((1,0), Tile(TILE_QUEEN))
-        self.model.add_tile((1,1), Tile())
-        self.model.add_tile((3,3), Tile())
         self.scale = 1.0
         self.offset_x = 0
         self.offset_y = 0
         self.last_pos = (0,0)
         self.dragging = False
+        self.place_candidates = []
+        self.place_type = -1
         self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
         self.Bind(wx.EVT_SIZE, self.on_size)
         self.Bind(wx.EVT_PAINT, self.on_paint)
@@ -147,7 +173,9 @@ class HivePanel(wx.Panel):
         self.Bind(wx.EVT_LEFT_UP, self.on_up)
         self.Bind(wx.EVT_MOUSEWHEEL, self.on_wheel)
         #self.Bind(wx.EVT_LEFT_CLICK, self.on_click)
-        
+        w,h = self.GetSize()
+        self.offset_x = -w/2
+        self.offset_y = -h/2
     def on_wheel(self, evt):
         rot = evt.GetWheelRotation()
         if rot > 0:
@@ -188,21 +216,35 @@ class HivePanel(wx.Panel):
             cy = self.offset_y + row*(s+h)
             dist = sqrt((cx-x)*(cx-x) + (cy-y)*(cy-y))
             if dist < r:
-                return HIT_TYPE_PLAY, (row, col)
+                return HIT_TYPE_TILE, (row, col)
 
+        for row,col in self.place_candidates:
+            cx = self.offset_x + col*a + r*(row%2)
+            cy = self.offset_y + row*(s+h)
+            dist = sqrt((cx-x)*(cx-x) + (cy-y)*(cy-y))
+            if dist < r:
+                return HIT_TYPE_EMPTY, (row, col)
         return HIT_TYPE_MISS, (-1,-1)
+
     def on_up(self, evt):
         self.dragging = False
         x,y = evt.GetPosition()
         type, hit = self.hit_test(x,y)
-        if(type == HIT_TYPE_PLAY):
+        if(type == HIT_TYPE_TILE):
             self.model.hilight_tile(hit)
+            self.place_candidates = []
+            self.place_type = -1
             self.Refresh()
         elif(type == HIT_TYPE_TILEBOX):
-            print "Got a tilebox hit: %d" % hit
+            self.place_candidates = self.model.get_legal_place_locs()
+            self.place_type = hit
+        elif(type == HIT_TYPE_EMPTY):
+            self.model.place_new_tile(hit, self.place_type)
+            self.place_candidates = []
+            self.place_type = -1
         else:
-            self.model.hilight_tile(None)
-
+            self.model.hilight_tile(None) 
+        self.Refresh()
     def draw_hex(self, dc, dims, x, y):
         h,s,r,a,b = dims
         x = x-r
@@ -232,6 +274,7 @@ class HivePanel(wx.Panel):
         self.tilebox_size = hex_size
         for tile_type in TILES:
             self.tilebox_centers[(x,y)] = tile_type
+            dc.SetPen(wx.Pen(wx.Colour(128,128,128), line_thickness*2 if tile_type == self.place_type else line_thickness))
             dc.SetBrush(wx.Brush(wx.Colour(*TILE_COLORS[tile_type])))
             self.draw_hex(dc, (h,s,r,a,b), x,y)
             y += b + b*TILEBOX_SPACING
@@ -245,11 +288,12 @@ class HivePanel(wx.Panel):
         dc.SetBrush(wx.WHITE_BRUSH)
         h,s,r,a,b = hex_dims(self.scale*TILE_SIZE)
         line_thickness = self.scale*BORDER_SIZE 
-        if self.model.hilighted_tile:
-            dc.SetBrush(wx.Brush(wx.Colour(200,255,200)))
-            dc.SetPen(wx.Pen(wx.Colour(128, 255, 128),line_thickness))
-            for row, col in self.model.get_legal_moves(self.model.hilighted_tile):
-                self.draw_hex(dc, (h,s,r,a,b), self.offset_x + col*a + r*(row%2), self.offset_y + row*(s+h))
+        
+        # Placement candidates
+        dc.SetBrush(wx.Brush(wx.Colour(200,255,200)))
+        dc.SetPen(wx.Pen(wx.Colour(128, 255, 128),line_thickness))
+        for row, col in self.place_candidates:
+            self.draw_hex(dc, (h,s,r,a,b), self.offset_x + col*a + r*(row%2), self.offset_y + row*(s+h))
 
         dc.SetPen(wx.Pen(wx.Colour(128,128,128), line_thickness))
         for (row,col), tile in self.model.visible_tiles.items():
